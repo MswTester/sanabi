9cb180e3d6cde2d3c2ef0b6dc845c176ef77b977
mswtester <mswtester10417@gmail.com>
2024-08-26 00:04:12 +0900

    aadd router

---
+++ app/api/get/route.ts
@@ -0,0 +1,9 @@
+import client from "../db";
+
+export async function GET(request: Request) {
+    const tokenCol = client.db("sanabi").collection("tokens");
+    const tokens = await tokenCol.find({}).toArray();
+    return new Response(JSON.stringify(tokens), {
+        headers: { "content-type": "application/json" },
+    });
+}
