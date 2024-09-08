cc74305ca9222a74ff25a0259893b4ea42a06c6e
mswtester <mswtester10417@gmail.com>
2024-09-09 01:05:16 +0900

    unicum 1.1.15

--- app/product/unicum/page.tsx
+++ app/product/unicum/page.tsx
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <Product
             id="unicum"
-            version="1.1.14"
+            version="1.1.15"
             title="Unicum"
             description="Dynamic cheat tools. It have many features that provide in-game API as electron web services."
             steps={[
