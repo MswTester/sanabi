8dd193b94dc0021cd8639d3604f29802d47f03d2
mswtester <mswtester10417@gmail.com>
2024-09-21 20:35:19 +0900

    pixel 1.2.1

--- app/product/pixel/page.tsx
+++ app/product/pixel/page.tsx
@@ -4,7 +4,7 @@ export default function Main() {
     return (
         <Product
             id="pixel"
-            version="1.2.0"
+            version="1.2.1"
             title="Pixel"
             description="Legacy cheat model. It's for researching and reversing the game."
             steps={[]}
